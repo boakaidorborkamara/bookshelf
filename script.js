edc069520ceb42c83656aebb9268332dcb7753cf
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-11-13 02:49:59 +0000

    Add script file and modal to add a new book

---
+++ script.js
@@ -0,0 +1,14 @@
+const Library = [{title:"Things Fall Apart", author:"James", pages:4, status:"Read"}];
+
+// book constructor 
+function Book(title, author, pages, status ){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+
+function createBook(){
+    
+}
